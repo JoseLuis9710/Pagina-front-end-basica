1b2ab1b835ceff04bc27dbf4a2c1b7b4b89a412e
JoseLuis9710 <73805898+JoseLuis9710@users.noreply.github.com>
2021-05-07 09:48:36 -0500

    Add files via upload

---
+++ JAVASCRIPT/java_admin.js
@@ -0,0 +1,27 @@
+function open1(evt, Name1) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(Name1).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+function open2(evt,Name) {
+  var i, tabcontent2, tablinks2;
+  tabcontent2 = document.getElementsByClassName("tabcontent2");
+  for (i = 0; i < tabcontent2.length; i++) {
+    tabcontent2[i].style.display = "none";
+  }
+  tablinks2 = document.getElementsByClassName("tablinks2");
+  for (i = 0; i < tablinks2.length; i++) {
+    tablinks2[i].className = tablinks2[i].className.replace(" active", "");
+  }
+  document.getElementById(Name).style.display = "block";
+  evt.currentTarget.className += " active";
+}
